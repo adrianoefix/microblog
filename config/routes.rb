70de31e6ab045ffdb5ab5ab9e307761fd97e5192
Ubuntu <adriano@ubuntu.(none)>
2012-11-24 04:56:13 -0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Microblog::Application.routes.draw do
 
+	resources :users
+	
 	root :to => 'static_pages#home'
 	
+	match '/signup' => 'users#new'
 	match '/help' =>  'static_pages#help'
   match '/about'=>  'static_pages#about'
   match '/contact' =>  'static_pages#contact'  
